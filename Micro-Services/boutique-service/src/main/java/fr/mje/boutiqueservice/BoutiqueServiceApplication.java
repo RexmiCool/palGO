8625fb9f6f86193073ac1d954a0ca2f85df8cb37
MaelJEAN <mael.jean@etu.mines-ales.fr>
2024-01-30 12:19:44 +0100

    boutique-service dockerize

---
+++ Micro-Services/boutique-service/src/main/java/fr/mje/boutiqueservice/BoutiqueServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.mje.boutiqueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoutiqueServiceApplication.class, args);
+    }
+
+}
